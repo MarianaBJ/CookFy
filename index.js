6e8cc3c570be93c18276fc595e03c08b70f7a1ea
Mariana <marianadiscipula@gmail.com>
2022-02-05 18:02:30 -0300

    PopUp feito

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ function openMenu(){
 function closeMenu(){
     document.getElementById('menu').style.width = '0px';
 }
+
+function popUp(id){
+    document.getElementById(id).style.display = 'block';
+}
+
+function closePopUp(id){
+    document.getElementById(id).style.display = 'none';
+}
+
+let img = document.querySelector('#social-footer-pin')
+img.setAttribute('src', './assets/social_midias/Pinterest_active.png')
+
+function roll(id){
+    document.getElementById(id).style.display = 'none'
+}
